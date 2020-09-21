4e2e501a42b87c2b218f282aa1939fe5b6820e48
github@cybertekschool.com <github@cybertekschool.com>
2020-09-21 15:20:56 -0500

    added polym class

---
+++ src/polymor/UsingDateTime.java
@@ -0,0 +1,13 @@
+package polymor;
+
+import java.time.LocalDate;
+
+public class UsingDateTime{
+			public static void main(String[] args) {
+				
+			}
+			LocalDate someBirthday= LocalDate.of(1996,5,21);
+			
+
+
+}
